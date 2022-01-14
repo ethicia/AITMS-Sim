5f8d95fa1f70c19034d7a31c70749131b3f45ffd
giwilorjelly <gwlgidwani@gmail.com>
2022-01-14 21:45:29 +0530

    AIT-37 #done

--- Assets/AITMS/Scripts/RecordHaltDuration.cs
+++ Assets/AITMS/Scripts/RecordHaltDuration.cs
@@ -13,19 +13,23 @@ namespace Kawaiiju
     {
 
         public UnityEngine.AI.NavMeshAgent agent;
-        public float movingSpeed;
+        public float movingSpeed = 1;
         public float minHaltDuration = 2.0f;
-        public Status status;
-
+        [HideInInspector] public Status status;
+        [HideInInspector] public float totalHaltDuration;
+        [HideInInspector] public int haltCount;
+        [HideInInspector] public float avgHaltDuration;
         private long stopTime;
-        private List<float> haltDurations;
+
 
         // Start is called before the first frame update
         void Start()
         {
             status = Status.Moving;
             agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-            haltDurations = new List<float>();
+            totalHaltDuration = 0;
+            haltCount = 0;
+            avgHaltDuration = 0;
         }
 
         // Update is called once per frame
@@ -56,13 +60,34 @@ namespace Kawaiiju
                             float haltDuration = (DateTimeOffset.Now.ToUnixTimeMilliseconds() - stopTime) / 1000.0f;
                             if (haltDuration >= minHaltDuration)
                             {
-                                Debug.Log(haltDuration);
-                                haltDurations.Add(haltDuration);
+                                totalHaltDuration += haltDuration;
+                                haltCount++;
+                                avgHaltDuration = totalHaltDuration / haltCount;
+
+                                //Debug.Log("Halt Duration: " + haltDuration);
+                                //Debug.Log("Total Duration: " + totalHaltDuration);
+                                //Debug.Log("Halt Count: " + haltCount);
+                                //Debug.Log("Avg Duration: " + avgHaltDuration);
+
                             }
                         }
                         break;
                     }
             }
+        }
+
+        public float getAvgHaltDuration()
+        {
+            return avgHaltDuration;
+        }
+
+        public void reset()
+        {
+            status = Status.Moving;
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            totalHaltDuration = 0;
+            haltCount = 0;
+            avgHaltDuration = 0;
         }
     }
 }
