4e61d2aeeab8743ffa2c53a3a82bafd5d88d3f19
Saquib Akhtar <saquib_ansari@outlook.com>
2022-02-02 00:35:49 +0530

    count added in script and prefab

--- Assets/AITMS/Scripts/Junction.cs
+++ Assets/AITMS/Scripts/Junction.cs
@@ -5,72 +5,97 @@ using UnityEngine;
 
 namespace Kawaiiju.Traffic
 {
-	public class Junction : Road 
-	{	
-		// -------------------------------------------------------------------
-		// Enum
+    public class Junction : Road
+    {
+        // -------------------------------------------------------------------
+        // Enum
 
-		public enum PhaseType { Timed, OnDemand }
+        public enum PhaseType { Timed, OnDemand }
 
-		// -------------------------------------------------------------------
-		// Properties
+        // -------------------------------------------------------------------
+        // Properties
 
-		[Header("Junction")]
-		public PhaseType type = PhaseType.Timed;
-		public Phase[] phases;
-		public JunctionTrigger[] triggers;
-		public float phaseInterval = 5;
+        [Header("Junction")]
+        public PhaseType type = PhaseType.Timed;
+        public Phase[] phases;
+        public JunctionTrigger[] triggers;
+        public float phaseInterval = 5;
+        public int firstLaneCount = 0;
+        public int secondLaneCount = 0;
+        public int thirdLaneCount = 0;
+        public int fourthLaneCount = 0;
+        [HideInInspector] public GameObject another;
+        public LaneVehicleCount firstLaneBox;
+        public LaneVehicleCount secondLaneBox;
+        public LaneVehicleCount thirdLaneBox;
+        public LaneVehicleCount fourthLaneBox;
+        // -------------------------------------------------------------------
+        // Initialization
 
-		// -------------------------------------------------------------------
-		// Initialization
+        public override void Start()
+        {
+            base.Start();
+            if (phases.Length > 0)
+                phases[0].Enable();
+            foreach (JunctionTrigger jt in triggers)
+                jt.junction = this;
+            //another = GameObject.FindGameObjectWithTag("FirstLane");
+            //firstLaneBox = another.GetComponent<LaneVehicleCount>();
+            //another = GameObject.FindGameObjectWithTag("SecondLane");
+            //secondLaneBox = another.GetComponent<LaneVehicleCount>();
+            //another = GameObject.FindGameObjectWithTag("ThirdLane");
+            //thirdLaneBox = another.GetComponent<LaneVehicleCount>();
+            //another = GameObject.FindGameObjectWithTag("FourthLane");
+            //fourthLaneBox = another.GetComponent<LaneVehicleCount>();
 
-		public override void Start()
-		{
-			base.Start();
-			if(phases.Length > 0)
-				phases[0].Enable();
-			foreach(JunctionTrigger jt in triggers)
-				jt.junction = this;
-		}
+        }
 
-		// -------------------------------------------------------------------
-		// Update
+        // -------------------------------------------------------------------
+        // Update
 
-		private void Update()
-		{
-			if(type == PhaseType.Timed)
-			{
-				m_PhaseTimer += Time.deltaTime;
-				if(!m_PhaseEnded && m_PhaseTimer > (phaseInterval * 0.5f))
-					EndPhase();
-				if(m_PhaseTimer > phaseInterval)
-					ChangePhase();
-			}
-		}
+        private void Update()
+        {
+            if (type == PhaseType.Timed)
+            {
+                m_PhaseTimer += Time.deltaTime;
+                if (!m_PhaseEnded && m_PhaseTimer > (phaseInterval * 0.5f))
+                    EndPhase();
+                if (m_PhaseTimer > phaseInterval)
+                    ChangePhase();
+            }
+            if(firstLaneBox != null)
+            firstLaneCount = firstLaneBox.vehiclewithin;
+            if(secondLaneBox != null)
+            secondLaneCount = secondLaneBox.vehiclewithin;
+            if(thirdLaneBox != null)
+            thirdLaneCount = thirdLaneBox.vehiclewithin;
+            if(fourthLaneBox != null)
+            fourthLaneCount = fourthLaneBox.vehiclewithin;
+        }
 
-		// -------------------------------------------------------------------
-		// Phase
+        // -------------------------------------------------------------------
+        // Phase
 
-		float m_PhaseTimer;
-		bool m_PhaseEnded;
-		private int m_CurrentPhase;
+        float m_PhaseTimer;
+        bool m_PhaseEnded;
+        private int m_CurrentPhase;
 
-		private void EndPhase()
-		{
-			m_PhaseEnded = true;
-			phases[m_CurrentPhase].End();
-		}
+        private void EndPhase()
+        {
+            m_PhaseEnded = true;
+            phases[m_CurrentPhase].End();
+        }
 
-		public void ChangePhase()
-		{
-			m_PhaseTimer = 0;
-			m_PhaseEnded = false;
-			if(m_CurrentPhase < phases.Length - 1)
-				m_CurrentPhase++;
-			else
-				m_CurrentPhase = 0;
-			phases[m_CurrentPhase].Enable();
-		}
+        public void ChangePhase()
+        {
+            m_PhaseTimer = 0;
+            m_PhaseEnded = false;
+            if (m_CurrentPhase < phases.Length - 1)
+                m_CurrentPhase++;
+            else
+                m_CurrentPhase = 0;
+            phases[m_CurrentPhase].Enable();
+        }
 
         public void TryChangePhase()
         {
@@ -78,75 +103,75 @@ namespace Kawaiiju.Traffic
                 ChangePhase();
         }
 
-		// -------------------------------------------------------------------
-		// Debug
+        // -------------------------------------------------------------------
+        // Debug
 
-		private Mesh m_Cube;
-		public Mesh cube
-		{
-			get
-			{
-				if(m_Cube == null)
-				{
-					GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					m_Cube = gameObject.GetComponent<MeshFilter>().sharedMesh;
-					GameObject.DestroyImmediate(gameObject);
-				}
-				return m_Cube;
-			}
-		}
+        private Mesh m_Cube;
+        public Mesh cube
+        {
+            get
+            {
+                if (m_Cube == null)
+                {
+                    GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    m_Cube = gameObject.GetComponent<MeshFilter>().sharedMesh;
+                    GameObject.DestroyImmediate(gameObject);
+                }
+                return m_Cube;
+            }
+        }
 
-		private void OnDrawGizmos()
-		{
-			if(TrafficSystem.Instance.drawGizmos)
-			{
-				Phase phase = phases[m_CurrentPhase];
-				foreach(WaitZone zone in phase.positiveZones)
-				{	
-					Gizmos.color = zone.canPass ? Color.green : Color.red;
-					DrawAreaGizmo(zone.transform);
-				}
-				Gizmos.color = Color.red;
-				foreach(WaitZone zone in phase.negativeZones)
-					DrawAreaGizmo(zone.transform);
-			}
-		}
+        private void OnDrawGizmos()
+        {
+            if (TrafficSystem.Instance.drawGizmos)
+            {
+                Phase phase = phases[m_CurrentPhase];
+                foreach (WaitZone zone in phase.positiveZones)
+                {
+                    Gizmos.color = zone.canPass ? Color.green : Color.red;
+                    DrawAreaGizmo(zone.transform);
+                }
+                Gizmos.color = Color.red;
+                foreach (WaitZone zone in phase.negativeZones)
+                    DrawAreaGizmo(zone.transform);
+            }
+        }
 
-		private void DrawAreaGizmo(Transform t)
-		{
-			Matrix4x4 rotationMatrix1 = Matrix4x4.TRS(t.position - new Vector3(0, t.localScale.y * 0.5f, 0), t.rotation, Vector3.Scale(t.lossyScale, new Vector3(1f, 0.1f, 1f)));
-			Gizmos.matrix = rotationMatrix1;
-			Gizmos.DrawWireMesh(cube, Vector3.zero, Quaternion.identity);
-		}
+        private void DrawAreaGizmo(Transform t)
+        {
+            Matrix4x4 rotationMatrix1 = Matrix4x4.TRS(t.position - new Vector3(0, t.localScale.y * 0.5f, 0), t.rotation, Vector3.Scale(t.lossyScale, new Vector3(1f, 0.1f, 1f)));
+            Gizmos.matrix = rotationMatrix1;
+            Gizmos.DrawWireMesh(cube, Vector3.zero, Quaternion.identity);
+        }
 
-		// -------------------------------------------------------------------
-		// Data Classes
+        // -------------------------------------------------------------------
+        // Data Classes
 
-		[Serializable]
-		public class Phase
-		{
-			public WaitZone[] positiveZones;
-			public WaitZone[] negativeZones;
-			public TrafficLight[] positiveLights;
-			public TrafficLight[] negativeLights;
+        [Serializable]
+        public class Phase
+        {
+            public WaitZone[] positiveZones;
+            public WaitZone[] negativeZones;
+            public TrafficLight[] positiveLights;
+            public TrafficLight[] negativeLights;
 
-			public void Enable()
-			{
-				foreach(WaitZone zone in positiveZones)
-					zone.canPass = true;
-				foreach(TrafficLight light in positiveLights)
-					light.SetLight(true);
-				foreach(WaitZone zone in negativeZones)
-					zone.canPass = false;
-				foreach(TrafficLight light in negativeLights)
-					light.SetLight(false);
-			}
+            public void Enable()
+            {
+                foreach (WaitZone zone in positiveZones)
+                    zone.canPass = true;
+                foreach (TrafficLight light in positiveLights)
+                    light.SetLight(true);
+                foreach (WaitZone zone in negativeZones)
+                    zone.canPass = false;
+                foreach (TrafficLight light in negativeLights)
+                    light.SetLight(false);
+            }
 
-			public void End()
-			{
-				foreach(WaitZone zone in positiveZones)
-					zone.canPass = false;
-			}
-		}
-	}
+            public void End()
+            {
+                foreach (WaitZone zone in positiveZones)
+                    zone.canPass = false;
+            }
+        }
+    }
 }
