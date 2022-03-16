46dd66a26a2ad2095dd15d5678b750a652fb84f0
Rutuja shinde <73520085+shinderutuja@users.noreply.github.com>
2022-03-17 01:26:51 +0530

    AIT-64 done

--- Assets/Code/Scripts/TrafficLight.cs
+++ Assets/Code/Scripts/TrafficLight.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 namespace Kawaiiju.Traffic
 {
-	public class TrafficLight : MonoBehaviour 
-	{
-		public MeshRenderer Renderer;
+    public class TrafficLight : MonoBehaviour
+    {
 
-		public void SetLight(bool input)
-		{
-			Renderer.material.SetColor("_Color", input ? Color.green : Color.red );
-		}
-	}
+        public Light RedLight;
+        public Light GreenLight;
+        public void SetLight(bool input)
+        {
+            if (input)
+            {
+                //green on
+                GreenLight.intensity = 1.5f;
+                RedLight.intensity = 0;
+            }
+            else
+            {
+                GreenLight.intensity = 0;
+                RedLight.intensity = 1;
+            }
+
+        }
+    }
 }
